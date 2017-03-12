ed5aaf04dfdab15763b9eb21a970b9a90cd01b68
Geraldine Caicedo Hidalgo <Geraldine Caicedo Hidalgo>
2017-03-11 22:45:07 -0500

    #2 Poker 2 and 3 Test, Report in Spanish finish

--- Source Code/CS-WS02/src/Logic/Gem.java
+++ Source Code/CS-WS02/src/Logic/Gem.java
@@ -24,6 +24,7 @@ public class Gem {
 
     static final long module = 2147483647L;
     static final long multiplier = 48271L;
+    static final long q = 44488L;
     static final long increase = 3399L;
 
     // Global Variable, initialice the seed with multiplier value diferent from 0
@@ -41,12 +42,18 @@ public class Gem {
      */
     private double generateRandomGem() {
         double generated_number = 0.0;
-
-        long ho = (multiplier * r_seed) + increase;
-        r_seed  = ho % module;
-
+        
+        //This is for the big number multiplication overflow
+        long hi = r_seed / q;
+        long lo = r_seed - q * hi;
+        long t = multiplier * lo - increase * hi;
+        if (t > 0) {
+            r_seed = t;
+        } else {
+            r_seed = t + module;
+        }
         generated_number = ((double) r_seed / (double) module);
-
+        
         return generated_number;
     }
     
@@ -125,10 +132,8 @@ public class Gem {
     /**
      * Calculates if generator is good with the chi squared test
      * @param radom_numbers
-     * @param generator
-     * @return a string with the info
      */
-    private void chiSquaredTest(List<Double> radom_numbers, int generator){
+    private void chiSquaredTest(List<Double> radom_numbers){
                 
         double critical_chi_squared = 0;                                    
         double total_numbers = radom_numbers.size();                        //total number of random numbers
@@ -168,18 +173,112 @@ public class Gem {
      
     }
     
-    private List<String> pokerTwoTest(){
-        List<String> information = new LinkedList();
-        
-        return information;        
+     /**
+     * Format the numbers to a douuble with a given number of decimals
+     *
+     * @param list list of pseudo-random numbers
+     * @param decimals amount of decimals accepted
+     * @return list of formated pseudo-random numbers
+     */
+    private List<Double> formatList(List<Double> list, String decimals) {
+        List<Double> newList = new LinkedList<>();
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.US);
+        otherSymbols.setDecimalSeparator('.');
+        otherSymbols.setGroupingSeparator('.');
+        DecimalFormat decimal = new DecimalFormat(decimals, otherSymbols);
+        decimal.setRoundingMode(RoundingMode.UP);
+        for (int i = 0; i < list.size(); i++) {
+            newList.add(Double.parseDouble(decimal.format(list.get(i))));
+        }
+        return newList;
     }
     
-    private List<String> pokerThreeTest(){
-        List<String> information = new LinkedList();
+    /**
+     * Calculates if generator is good with the poker two test
+     * @param radom_numbers
+     */
+    private void pokerTwoTest(List<Double> radom_numbers){
+      
+        List<Double> information = formatList(radom_numbers, "#.##");
+        System.out.println("Class \t\t\t\t OF \t\t EF \t\t x2");
+        int equals_two = 0;
+        int equals_zero = 0;
+        double critical_chi_squared = 2.7055;           //calculates with the chi squared table and the degrees of freedom
+
+        for (Double number : information) {
+            double num = ceil(number * 100);
+            double num_1 = ((int) num / 10);
+            double num_2 = num % 10;
+            if (num_1 == num_2) {                      //The two decimals are equals
+                equals_two++;
+            } else {                                   //The two decimals are diferents
+                equals_zero++;
+            }
+        }
         
-        return information;        
+        double expected_frequency = information.size();     //calculates the expected frequency
+        double chi_2_1 = (pow((expected_frequency * 0.1) - equals_two, 2)) / (expected_frequency * 0.1);
+        double chi_2_2 = (pow((expected_frequency * 0.9) - equals_zero, 2)) / (expected_frequency * 0.9);
+        double calculated_chi_squared = chi_2_1 + chi_2_2;  //calculates the calculated chi squared
+        System.out.println("2 Equals      \t\t\t " + equals_two + " \t\t " + expected_frequency * 0.1 + " \t\t " + chi_2_1);
+        System.out.println("2 Diferents   \t\t\t " + equals_zero + " \t\t " + expected_frequency * 0.9 + " \t\t " + chi_2_2);
+        System.out.println("........................................................");
+        System.out.println("Calculated Chi Squared:        " + calculated_chi_squared);
+        System.out.println("Critical Chi Squared           " + critical_chi_squared);
+        
+        boolean good_generator = calculated_chi_squared <= critical_chi_squared;            //set if the generator is good  
+        System.out.println("Past the Test?: "+good_generator);
     }
     
+    /**
+     * Calculates if generator is good with the poker three test
+     * @param radom_numbers
+     */
+    private void pokerThreeTest(List<Double> radom_numbers){
+        
+        List<Double> information = formatList(radom_numbers, "#.###");//define el formato para 3 decimales
+        System.out.println("Class \t\t\t\t OF \t\t EF \t\t x2");
+        int equals_three = 0;
+        int equals_two = 0;
+        int equals_zero = 0;
+        double critical_chi_squared = 4.6052;           //calculates with the chi squared table and the degrees of freedom
+
+        for (Double number : information) {
+            double num = ceil(number * 1000);
+            double num1 = (int) num / 100;
+            double num2 = ((int) num / 10) % 10;
+            double num3 = num % 10;
+            if (num1 == num2 && num1 == num3) {         //the three numbers are equals
+                equals_three++;
+            } else if (num1 != num2 && num1 != num3 && num2 != num3) {      //the three numbers are diferents
+                equals_zero++;
+            } else {                                                        //there are two equals numbers
+                equals_two++;
+            }
+        }
+        double expected_frequency = information.size();                 //calculates the expected frequency
+        double chi_2_1 = (pow((expected_frequency * 0.01) - equals_three, 2)) / (expected_frequency * 0.01);
+        double chi_2_2 = (pow((expected_frequency * 0.27) - equals_two, 2)) / (expected_frequency * 0.27);
+        double chi_2_3 = (pow((expected_frequency * 0.72) - equals_zero, 2)) / (expected_frequency * 0.72);
+        double calculated_chi_squared = chi_2_1 + chi_2_2 + chi_2_3;    //calculates the calculated chi squared
+        System.out.println("3 Equals                \t " + equals_three + " \t\t " + expected_frequency * 0.01 + " \t\t " + chi_2_1);
+        System.out.println("2 Equals, 1 Diferent   \t " + equals_two + " \t\t " + expected_frequency * 0.27 + " \t\t " + chi_2_2);
+        System.out.println("3 Diferents             \t " + equals_zero + " \t\t " + expected_frequency * 0.72 + " \t\t " + chi_2_3);
+        System.out.println("........................................................");
+        System.out.println("Calculated Chi Squared:        " + calculated_chi_squared);
+        System.out.println("Critical Chi Squared           " + critical_chi_squared);
+        
+        boolean good_generator = calculated_chi_squared <= critical_chi_squared;            //set if the generator is good  
+        System.out.println("Past the Test?: "+good_generator);    
+    }
+    
+    /**
+     * Print the information with the test, generator and number of pseudo-random numbers given
+     * @param goodness_of_fit
+     * @param generator
+     * @param number
+     * @throws IOException 
+     */
     public void printInformation(int goodness_of_fit, int generator, int number) throws IOException {
         
         List<Double> random_numbers = generateListRandomNumbers(generator);             //set the random numbers list according to generator
@@ -191,41 +290,52 @@ public class Gem {
         // Chi Squared Test choosen
         if(goodness_of_fit == 1){
             System.out.println("--------------------- Chi Squared Test ---------------------");
-            chiSquaredTest(random_numbers,generator);
+            chiSquaredTest(random_numbers);
         
         // Poker 2 Test choosen
         }else if(goodness_of_fit == 2){
             System.out.println("--------------------- Poker 2 Test ---------------------");
+            pokerTwoTest(random_numbers);
             
         // Poker 3 Squared Test choosen
         }else if(goodness_of_fit == 3){
             System.out.println("--------------------- Poker 3 Test ---------------------");
-            
+            pokerThreeTest(random_numbers);
         }
     }
 
+    /**
+     * Main method
+     * @param args
+     * @throws IOException 
+     */
     public static void main(String[] args) throws IOException {
 
         Gem gem = new Gem();
         
         // Goodness of Fit Chi² of GEM Generator
-        gem.printInformation(1,0,1000);
+        //gem.printInformation(1,0,1000);
+        //gem.printInformation(1, 0, 10000);
         
         // Goodness of Fit Poker 2 of GEM Generator
-        //gem.printInformation(2,0);
+        //gem.printInformation(2,0,1000);
+        //gem.printInformation(2,0,10000);
         
         // Goodness of Fit Poker 3 of GEM Generator
-        //gem.printInformation(3,0);
+        //gem.printInformation(3,0,1000);
+        //gem.printInformation(3,0,10000);
         
         // Goodness of Fit Chi² of Java Generator
-        //gem.printInformation(1,1);
+        //gem.printInformation(1,1,1000);
+        //gem.printInformation(1,1,10000);
         
         // Goodness of Fit Poker 2 of Java Generator
-        //gem.printInformation(2,1);
+        gem.printInformation(2,1,1000);
+        gem.printInformation(2,1,10000);
         
         // Goodness of Fit Poker 3 of Java Generator
-        //gem.printInformation(3,1);
-
+        gem.printInformation(3,1,1000);
+        gem.printInformation(3,1,10000);
     }
 
 }
